8240e8238f06b6d57b0296d09207fbc61439a798
madhukumar1 <madhukumar2k@gmail.com>
2025-09-06 18:02:54 +0530

    a

---
+++ modules/aws/first_ec2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+
+resource "aws_instance" "myec2" {
+    ami = "ami-09e6f87a47903347c"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "MyEC2Instance"
+    }
+  
+}
