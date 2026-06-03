e4d498aea34951726b0dcf7eccac6c6c685a8e1a
Madhukumar1 <madhukumar2k@gmail.com>
2026-06-03 20:02:25 +0530

    Change S3 backend region from ap-south-1 to us-east-1

--- modules/aws/backend.tf
+++ modules/aws/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-123"
     key            = "terraform-project/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
