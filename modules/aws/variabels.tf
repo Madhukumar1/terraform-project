dbd15f717f0a3889daf7a0096ddc1322d82c1e2d
Madhukumar1 <madhukumar2k@gmail.com>
2026-06-16 16:42:06 +0530

    Update variabels.tf

--- modules/aws/variabels.tf
+++ modules/aws/variabels.tf
@@ -2,3 +2,13 @@ variable "key_name" {
   description = "Name of the existing AWS Key Pair to use for EC2 instance"
   type        = string
 }
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
