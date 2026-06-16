9de3765c3d1bc6562ff7c2180b0c44fa9c447212
Madhukumar1 <madhukumar2k@gmail.com>
2026-06-16 16:11:52 +0530

    Update S3 bucket and region in backend configuration

--- modules/aws/backend.tf
+++ modules/aws/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-123"
+    bucket         = "my-tf-test123-bucket12345"
     key            = "terraform-project/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
